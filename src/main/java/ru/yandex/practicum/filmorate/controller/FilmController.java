f95c5904ca43ca918c9714af4d92f679129a7ce4
Pavel Appolonov <app.pavel1998@gmail.com>
2024-09-25 20:02:41 +0300

    Добавить реализацию ТЗ спринта 10*

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,37 +3,40 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.controller.utils.FilmUtils;
 import ru.yandex.practicum.filmorate.exception.ValidateException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
+@Component
 @RequestMapping("/films")
 public class FilmController {
     private static final Logger log = LoggerFactory.getLogger(FilmController.class);
-    private final Map<Long, Film> films = new HashMap<>();
+
+    @Autowired
+    private FilmUtils filmUtils;
 
     @GetMapping
     public Collection<Film> showAll() {
-        return films.values();
+        return filmUtils.getFilms().values();
     }
 
     @PostMapping
     public Film addFilm(@Valid @RequestBody Film film) {
         Film newFilm;
         try {
-            newFilm = checkFilm(film);
+            newFilm = filmUtils.checkFilm(film);
         } catch (ValidateException e) {
             log.error(e.getMessage());
             throw e;
         }
-        newFilm.setId(getNextId());
-        films.put(newFilm.getId(), newFilm);
+        newFilm.setId(filmUtils.getNextId());
+        filmUtils.getFilms().put(newFilm.getId(), newFilm);
         log.info("Фильм {} был успешно добавлен", newFilm);
         return newFilm;
     }
@@ -42,7 +45,7 @@ public class FilmController {
     public Film updateFilm(@Valid @RequestBody Film film) {
         Film updatedFilm;
         try {
-            updatedFilm = checkFilm(film);
+            updatedFilm = filmUtils.checkFilm(film);
         } catch (ValidateException e) {
             log.error(e.getMessage());
             throw e;
@@ -50,8 +53,8 @@ public class FilmController {
         if (updatedFilm.getId() == null) {
             throw new ValidateException("ID фильма должен быть указан");
         }
-        if (films.containsKey(updatedFilm.getId())) {
-            Film oldFilm = films.get(updatedFilm.getId());
+        if (filmUtils.getFilms().containsKey(updatedFilm.getId())) {
+            Film oldFilm = filmUtils.getFilms().get(updatedFilm.getId());
             if (oldFilm.equals(updatedFilm)) {
                 log.info("Фильм {} идентичен с обновляемым", updatedFilm);
                 return oldFilm;
@@ -64,28 +67,5 @@ public class FilmController {
             return oldFilm;
         }
         throw new ValidateException("Film с ID " + updatedFilm.getId() + " не найден");
-    }
-
-    private Film checkFilm(Film film) {
-        if (film.getDescription().length() > 200) {
-            throw new ValidateException("Превышен лимит символов");
-        }
-        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidateException("Дата релиза не должна быть позже 28 декабря 1895 года ");
-        }
-        if (!(film.getDuration().isPositive())) {
-            throw new ValidateException("Продолжительность должна положительным числом");
-        }
-        log.info("Фильм прошел проверку полей");
-        return film;
-    }
-
-    private long getNextId() {
-        long currentMaxId = films.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,37 +3,38 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.controller.utils.UserUtils;
 import ru.yandex.practicum.filmorate.exception.ValidateException;
 import ru.yandex.practicum.filmorate.model.User;
 
-import java.time.LocalDate;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
-    private final Map<Long, User> users = new HashMap<>();
+
+    @Autowired
+    private UserUtils userUtils;
 
     @GetMapping
     public Collection<User> showAll() {
-        return users.values();
+        return userUtils.getUsers().values();
     }
 
     @PostMapping
     public User addUser(@Valid @RequestBody User user) {
         User newUser;
         try {
-            newUser = checkUser(user);
+            newUser = userUtils.checkUser(user);
         } catch (ValidateException e) {
             log.error(e.getMessage());
             throw e;
         }
-        newUser.setId(getNextId());
-        users.put(newUser.getId(), newUser);
+        newUser.setId(userUtils.getNextId());
+        userUtils.getUsers().put(newUser.getId(), newUser);
         log.info("Пользователь {} был успешно добавлен", newUser);
         return newUser;
     }
@@ -42,7 +43,7 @@ public class UserController {
     public User updateUser(@Valid @RequestBody User user) {
         User updatedUser;
         try {
-            updatedUser = checkUser(user);
+            updatedUser = userUtils.checkUser(user);
         } catch (ValidateException e) {
             log.error(e.getMessage());
             throw e;
@@ -50,8 +51,8 @@ public class UserController {
         if (updatedUser.getId() == null) {
             throw new ValidateException("ID пользователя должен быть указан");
         }
-        if (users.containsKey(updatedUser.getId())) {
-            User oldUser = users.get(updatedUser.getId());
+        if (userUtils.getUsers().containsKey(updatedUser.getId())) {
+            User oldUser = userUtils.getUsers().get(updatedUser.getId());
             if (oldUser.equals(updatedUser)) {
                 log.info("Обновляемые данные идентичны старым");
                 return oldUser;
@@ -64,29 +65,5 @@ public class UserController {
             return oldUser;
         }
         throw new ValidateException("User с ID " + updatedUser.getId() + " не найден");
-    }
-
-    private User checkUser(User user) {
-        if (user.getEmail() == null || !user.getEmail().contains("@")) {
-            throw new ValidateException("Неверный формат email");
-        }
-        if (user.getName() == null || user.getName().isBlank()) {
-            log.debug("Пустое поле Name было изменено на логин {}", user.getLogin());
-            user.setName(user.getLogin());
-        }
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            throw new ValidateException("Дата рождения введена неверно");
-        }
-        log.info("Пользователь прошел проверку полей");
-        return user;
-    }
-
-    private long getNextId() {
-        long currentMaxId = users.keySet()
-                .stream()
-                .mapToLong(id -> id)
-                .max()
-                .orElse(0);
-        return ++currentMaxId;
     }
 }
