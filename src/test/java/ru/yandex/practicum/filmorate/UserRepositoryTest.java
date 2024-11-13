74cfcc9527b40f928f1145374d14570640250f34
Pavel Appolonov <app.pavel1998@gmail.com>
2024-11-13 12:10:36 +0300

    Реализиция спринта 12

--- src/test/java/ru/yandex/practicum/filmorate/UserRepositoryTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserRepositoryTest.java
@@ -1,8 +1,5 @@
 package ru.yandex.practicum.filmorate;
 
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,11 +7,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import ru.yandex.practicum.filmorate.dal.FilmRepositories.FilmRepository;
 import ru.yandex.practicum.filmorate.dal.UserRerositories.UserRepository;
-import ru.yandex.practicum.filmorate.dal.mappers.FilmRowMapper;
 import ru.yandex.practicum.filmorate.dal.mappers.UserRowMapper;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.time.LocalDate;
